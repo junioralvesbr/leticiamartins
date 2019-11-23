c17f4ab91b78c53bd524d42c57a1c82620f352b2
Junior Alves <jrnalves@gmail.com>
2019-11-23 16:18:00 -0300

    Refatorado e ajustado a mudança de cor do menu pelo scroll

--- src/menu-smoothScroll.js
+++ src/menu-smoothScroll.js
@@ -1,4 +1,5 @@
-const $sections = document.querySelectorAll('.sections');
+const $section = document.querySelectorAll('.section');
+const $menu = document.querySelectorAll('.nav-menu .item')
 const $menuItems = document.querySelectorAll('.list .action[href^="#"]');
 
 const scrollToIdOnClick = (event) => {
@@ -23,4 +24,28 @@ $menuItems.forEach(item => {
     item.addEventListener('click', scrollToIdOnClick);
 });
 
-console.log($sections[3].offsetTop)
+const addRemoveActive = (numberSection) => {
+    console.log(numberSection);
+        $menu.forEach($item => $item.classList.remove('-active'))
+        $menu[numberSection].classList.add('-active')
+}
+
+window.addEventListener('scroll', () => {
+    let scroll = (window.scrollY + 500)
+    
+    if ((scroll > $section[0].offsetTop) && (scroll < $section[1].offsetTop)) {
+        addRemoveActive(0)
+
+    } else if ((scroll > $section[1].offsetTop) && (scroll < $section[2].offsetTop)) {
+        addRemoveActive(1)
+
+    } else if ((scroll > $section[2].offsetTop) && (scroll < $section[3].offsetTop)) {
+        addRemoveActive(2)
+
+    } else if ((scroll > $section[3].offsetTop) && (scroll < $section[4].offsetTop)) {
+        addRemoveActive(3)
+
+    } else {
+        addRemoveActive(4)
+    }
+})
