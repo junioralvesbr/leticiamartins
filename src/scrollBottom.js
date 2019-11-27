9563338ec3ffc547c6f43d243229f16b022df58d
Junior Alves <jrnalves@gmail.com>
2019-11-27 17:19:10 -0300

    Inserido a função de scroll no sevices, about e contact

---
+++ src/scrollBottom.js
@@ -0,0 +1,20 @@
+const $target = document.querySelectorAll('[data-anime]')
+const animationClass = 'animate';
+
+function animeScroll() {
+    const windowTop = window.scrollY + ((window.innerHeight * 3) / 4);
+
+    $target.forEach(function(element){
+        if (windowTop > element.offsetTop) {
+            element.classList.add(animationClass);
+        } else {
+            element.classList.remove(animationClass);
+        }
+    })
+}
+
+if ($target.length) {
+    window.addEventListener('scroll', function() {
+        animeScroll();
+    })
+}
